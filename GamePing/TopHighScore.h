080074cbbee98447234fe6ac8a42604a602a518e
nltd101 <nltd101@gmail.com>
2019-12-08 18:30:28 +0700

    connect server get highscore

---
+++ GamePing/TopHighScore.h
@@ -0,0 +1,12 @@
+#pragma once
+#include "SFML/Network.hpp"
+#include <iostream>
+#include "Score.h"
+#include "vector"
+using namespace std;
+class TopHighScore
+{
+public:
+	string getHighScore();
+};
+
