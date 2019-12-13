638541f0f1079565b30058600ee9bbbd9bd50ba7
nltd101 <nltd101@gmail.com>
2019-12-13 17:46:13 +0700

    return the number of brick

--- GamePing/GenerateMapBaseLevel.cpp
+++ GamePing/GenerateMapBaseLevel.cpp
@@ -3,8 +3,9 @@
 GenerateMapBaseLevel::GenerateMapBaseLevel()
 {
 }
-void GenerateMapBaseLevel::setMapLevel(int temp[9][10], int level)
+int GenerateMapBaseLevel::setMapLevel(int temp[9][10], int level)
 {
+	int countBrick;
 	int a[7][10][9] =
 	{ {{0,1,0,0,1,0,1,1,0},
 		{1,0,1,1,0,1,1,0,1},
@@ -46,9 +47,14 @@ void GenerateMapBaseLevel::setMapLevel(int temp[9][10], int level)
 		for (int j = 0; j < 10; j++)
 		{
 			temp[i][j] = a[level][j][i];
+			if (temp[i][j]!=0)
+			{
+				countBrick++;
+			}
 		}
 	}
 	GenerateItem(temp);
+	return countBrick;
 }
 void GenerateMapBaseLevel::GenerateItem(int a[9][10])
 {
